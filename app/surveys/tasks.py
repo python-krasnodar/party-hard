cd54912c4eda5c97d79170b2f5d0dcf7c61ddbb0
Memory Clutter <memclutter@gmail.com>
2017-09-24 15:26:33 +0300

    Send email notification in background

--- app/surveys/tasks.py
+++ app/surveys/tasks.py
@@ -1,8 +1,12 @@
+"""Surveys tasks module"""
+from django.core.mail import EmailMessage
+from django.utils.translation import gettext as _
 from celery import shared_task
 from config.settings import ORG_EMAIL
 
 
 @shared_task
 def send_org_email(message):
-    print(message)
-    pass
+    """Send email notification in background task"""
+    email = EmailMessage(_('Party Hard Notification'), message, to=[ORG_EMAIL])
+    email.send()
